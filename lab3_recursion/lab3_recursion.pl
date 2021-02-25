fe32f7a65159a506dbd2c65e47f4e08f88c28ce5
ioilcon <Zdvadim98@gmail.com>
2021-02-25 16:50:30 +0300

    Задание 5 и 6

--- lab3_recursion/lab3_recursion.pl
+++ lab3_recursion/lab3_recursion.pl
@@ -5,12 +5,18 @@ max(X,Y,U,X):-X>Y,X>U,!.
 max(_,Y,U,Y):-Y>U,!.
 max(_,_,U,U).
 
-fact(0,1):-!.
-fact(N,X):-N1 is N-1,fact(N1,X1),X is X1*N.
+fact_up(0,1):-!.
+fact_up(N,X):-N1 is N-1,fact_up(N1,X1),X is X1*N.
 
-fact_d(N,X,N,X):-!.
-fact_d(I,F,N,X):-I1 is I+1,F1 is F*I1,fact_d(I1,F1,N,X).
+fact_down(N,X,N,X):-!.
+fact_down(I,F,N,X):-I1 is I+1,F1 is F*I1,fact_down(I1,F1,N,X).
 
-fact_d(N,X):-fact_d(0,1,N,X).
+fact_down(N,X):-fact_down(0,1,N,X).
 
+fib_up(1,1):-!.
+fib_up(2,1):-!.
+fib_up(N,X):-N1 is N-1,N2 is N-2,fib_up(N2,X2),fib_up(N1,X1),X is X1+X2.
 
+fib_down(N,X):-fib_down(N,1,1,X).
+fib_down(1,F2,_,F2):-!.
+fib_down(N,F1,F2,X):-N1 is N-1,NF2 is F1 + F2,fib_down(N1,F2,NF2,X).
