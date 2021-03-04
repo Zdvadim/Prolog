e5058445b5e7b2164eefe1e814654e1b3ef56313
ioilcon <Zdvadim98@gmail.com>
2021-03-05 01:44:55 +0300

    Задание 14

--- lab3_recursion/lab3_recursion.pl
+++ lab3_recursion/lab3_recursion.pl
@@ -90,3 +90,8 @@ pr_3_13(Cur,Border,Max,IMax,Res):-chain(Cur,Len),(Len>Max -> NIMax is Cur,NMax i
 chain(Num,Res):-chain(Num,0,Res).
 chain(1,Res,Res):-!.
 chain(Num,Len,Res):-(0 is Num mod 2 -> NNum is Num / 2;NNum is Num*3+1),NLen is Len+1,chain(NNum,NLen,Res).
+
+not_simple_divs(Num,Sum):-(is_simple(Num) -> Sum = 0;SSum is Num+1,not_simple_divs(Num,SSum,2,Sum)).
+not_simple_divs(Num,Sum,Num,Sum):-!.
+not_simple_divs(Num,CSum,Cur,Sum):-(0 is Num mod Cur,not(is_simple(Cur)) -> NCSum is CSum +Cur;NCSum is CSum),Next is Cur+1,not_simple_divs(Num,NCSum,Next,Sum).
+
